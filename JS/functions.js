61a9f446906583853f9cd1c5a64da02dcb2ca3cd
Jakub Kulik <jakub.kulik1@o2.pl>
2023-09-26 10:28:53 +0200

    add function buttons in JS

--- JS/functions.js
+++ JS/functions.js
@@ -10,30 +10,55 @@
       },
    ];
 
+   const addNewTask = (newTaskContent) => {
+
+      tasks.push({
+         content: newTaskContent,
+      });
+
+      render();
+   };
+
+   const removeTask = (taskIndex) => {
+      tasks.splice(taskIndex, 1);
+      render();
+   };
+   const toggleTaskDone = (taskIndex) => {
+      tasks[taskIndex].done = !tasks[taskIndex].done;
+      render();
+   };
+
+
    const render = () => {
       let htmlString = "";
 
       for (const task of tasks) {
          htmlString += `
             <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
+            <button class="js-done"> zrobione?</button>
+            <button class="js-remove"> Usuń </button>
             ${task.content}
             </li>
             `;
       }
       document.querySelector(".js-tasks").innerHTML = htmlString;
-   };
 
-   const addNewTask = (newTaskContent) => {
+      const removeButtons = document.querySelectorAll(".js-remove");
 
-      tasks.push({
-         content: newTaskContent,
-         done: false,
+      removeButtons.forEach((removeButtons, taskIndex) => {
+         removeButtons.addEventListener("click", () => {
+            removeTask();
+         });
       });
 
-      render();
+      const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+      toggleDoneButtons.forEach((toggleDoneButton, taskIndex) => {
+         toggleDoneButton.addEventListener("click", () => {
+            toggleTaskDone();
+         });
+      });
    };
-
-
 
    const onFormSubmit = (event) => {
       event.preventDefault();
