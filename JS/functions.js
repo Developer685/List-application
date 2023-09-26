5bdaeb6c41c96fb4a7dc73b48355e314b0a6e88e
Jakub Kulik <jakub.kulik1@o2.pl>
2023-09-26 11:48:23 +0200

    added new function bindEvents

--- JS/functions.js
+++ JS/functions.js
@@ -28,26 +28,12 @@
       render();
    };
 
-
-   const render = () => {
-      let htmlString = "";
-
-      for (const task of tasks) {
-         htmlString += `
-            <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
-            <button class="js-done"> zrobione?</button>
-            <button class="js-remove"> Usuń </button>
-            ${task.content}
-            </li>
-            `;
-      }
-      document.querySelector(".js-tasks").innerHTML = htmlString;
-
+   const bindEvents = () => {
       const removeButtons = document.querySelectorAll(".js-remove");
 
       removeButtons.forEach((removeButtons, taskIndex) => {
          removeButtons.addEventListener("click", () => {
-            removeTask();
+            removeTask(taskIndex);
          });
       });
 
@@ -55,9 +41,27 @@
 
       toggleDoneButtons.forEach((toggleDoneButton, taskIndex) => {
          toggleDoneButton.addEventListener("click", () => {
-            toggleTaskDone();
+            toggleTaskDone(taskIndex);
          });
       });
+   }
+
+
+   const render = () => {
+      let htmlString = "";
+
+      for (const task of tasks) {
+         htmlString += `
+            <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
+            <button class="js-done"> zrobione </button>
+            <button class="js-remove"> Usuń </button>
+            ${task.content}
+            </li>
+            `;
+      }
+      document.querySelector(".js-tasks").innerHTML = htmlString;
+
+      bindEvents();
    };
 
    const onFormSubmit = (event) => {
